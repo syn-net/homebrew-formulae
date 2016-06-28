49444c27fcaa008cf75a1ee3153fb572caa5127c
Jeffrey Carpenter <i8degrees@gmail.com>
2016-06-28 00:54:08 -0500

    Add McGimp formula

---
+++ Formula/casks/mcgimp.rb
@@ -0,0 +1,11 @@
+cask 'mcgimp' do
+  version '2.9.3'
+  sha256 '898e98082410db2ab1bcca5c8ffcea2dd3f75798e304d80146976f92e0b684df'
+
+  url 'http://www.partha.com/downloads/McGimp-2.9.app.zip'
+  name 'McGIMP'
+  homepage 'http://www.partha.com'
+  license :gpl
+  app 'McGimp-2.9.app'
+
+end
