f32cd5f66489b7b61c9bd813ed6bbbfa44644f87
Jeffrey Carpenter <i8degrees@gmail.com>
2016-08-19 09:26:21 -0500

    mcgimp.rb: Fix app name

--- Casks/mcgimp.rb
+++ Casks/mcgimp.rb
@@ -1,10 +1,10 @@
 cask 'mcgimp' do
-  version '2.9.5-cc'
+  version '2.9.5'
   sha256 '299f3f4345a508091b0d6f8d6bfd7f3f9495fe674d610178ca54599b911e4938'
 
   url 'http://www.partha.com/downloads/McGimp-2.9-cc.app.zip'
   name 'McGIMP'
   homepage 'http://www.partha.com'
   license :gpl
-  app 'McGimp-2.9.app'
+  app 'McGimp-2.9-cc.app'
 end
