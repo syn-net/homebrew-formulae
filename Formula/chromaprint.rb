326c76f93dd9a0f8b55c8a15ccdd6cd74b9a9ec3
Jeffrey Carpenter <i8degrees@gmail.com>
2017-12-30 00:33:26 -0600

    Add reference note

--- Formula/chromaprint.rb
+++ Formula/chromaprint.rb
@@ -1,3 +1,4 @@
+# Sourced from https://github.com/Homebrew/homebrew-core/pull/14235
 class Chromaprint < Formula
   desc "Core component of the AcoustID project (Audio fingerprinting)"
   homepage "https://acoustid.org/chromaprint"
