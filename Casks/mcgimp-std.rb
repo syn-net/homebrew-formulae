69fa8baf2451934fac8828414cb71bca2d5a69ae
Jeffrey Carpenter <i8degrees@gmail.com>
2018-04-06 12:16:41 -0500

    Update mcgimp-std.rb to v2.10

--- Casks/mcgimp-std.rb
+++ Casks/mcgimp-std.rb
@@ -1,20 +1,20 @@
 cask 'mcgimp-std' do
-  version '2.9.89'
-  sha256 '95a7e2d1e53423e92c88aac2cdcd5f2a886eded94353aecc1edfd1b01fdc3eb4'
+  version '2.10'
+  sha256 '3a73df217efaa2a53913e1f0884e8e3d4377b53faf0a9c885ad4250903297afb'
 
-  url 'http://www.partha.com/downloads/McGimp-2.9-std.app.zip'
+  url 'https://www.partha.com/downloads/McGimp-2.10-std.app.zip'  
   name 'GIMP'
   homepage 'http://www.partha.com'
 
-  app 'McGimp-2.9-std.app'
+  app 'McGimp-2.10-std.app'
 
   postflight do
-    set_permissions "#{appdir}/McGimp-2.9-std.app/Contents/MacOS/mcgimp-std",
+    set_permissions "#{appdir}/McGimp-2.10-std.app/Contents/MacOS/mcgimp-std",
       'a+rx'
   end
 
   zap trash: [
-    '~/Library/Preferences/gimp-2.9.plist',
+    '~/Library/Preferences/gimp-2.10.plist',
     # '~/Library/Application Support/GIMP',
   ]
 
