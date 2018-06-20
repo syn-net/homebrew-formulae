61c25b75952562409be9b8d35ec6533d0c192901
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2018-06-20 03:10:11 -0500

    Add brew-cask formula miro-video-converter

---
+++ Casks/miro-video-converter.rb
@@ -0,0 +1,12 @@
+# Origin: https://github.com/Homebrew/homebrew-cask/blob/e2c225f679d8895f93aa8ce1b6a8c3eb76080bf2/Casks/miro-video-converter.rb
+cask 'miro-video-converter' do
+  version '3.0'
+  sha256 '32f5b7fdfc8eb0892b76b33ed2ec92bc580b62b0e713cd300ee0e7a5de220ccb'
+
+  url 'http://getmiro.com/files/MiroVideoConverter.dmg'
+  name 'Miro Video Converter'
+  homepage 'http://www.mirovideoconverter.com/'
+  license :gpl
+
+  app 'Miro Video Converter.app'
+end
