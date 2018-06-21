a044a52383ee1178ceb9b25933388c03646809f0
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2018-06-21 05:01:11 -0500

    Add brew cask formula jubler

---
+++ Casks/jubler.rb
@@ -0,0 +1,20 @@
+cask 'jubler' do
+  version '6.0.pre6'
+  sha256 '4c129bae8bfa6af2fb01a2414f7e0f6cefe1c87ea1c129c7b00949a5a2c750a2'
+
+  # PLEASE NOTE: for macOS 10.13, there is a problem with all Java
+  # applications: please use the following link to download a beta version
+  # to solve this issue.
+  # sourceforge.net/jubler was verified as official when first introduced to the cask
+  url "https://github.com/teras/Jubler/releases/download/#{version}/Jubler-#{version}.dmg"
+
+  appcast 'https://sourceforge.net/projects/jubler/rss'
+  name 'Jubler'
+  homepage 'http://www.jubler.org/'
+
+  app 'Jubler.app'
+
+  caveats do
+    depends_on_java
+  end
+end
