47db0e1eb859c637c9738e03e2bd1b7d1c447d73
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2022-10-25 16:50:09 -0500

    Merge in changes from homebrew-core/nut

--- Formula/nut.rb
+++ Formula/nut.rb
@@ -1,97 +1,131 @@
 class Nut < Formula
   desc "Network UPS Tools: Support for various power devices"
   homepage "https://networkupstools.org/"
-  url "https://networkupstools.org/source/2.7/nut-2.7.4.tar.gz"
-  sha256 "980e82918c52d364605c0703a5dcf01f74ad2ef06e3d365949e43b7d406d25a7"
+  version "2.8.0"
+  license "GPL-2.0-or-later"
+
+  stable do
+    url "https://github.com/networkupstools/nut/releases/download/v#{version}/nut-#{version}.tar.gz"
+    sha256 "980e82918c52d364605c0703a5dcf01f74ad2ef06e3d365949e43b7d406d25a7"
+
+    # fix build failure
+    # remove in next release
+    patch do
+      url "https://github.com/networkupstools/nut/commit/9e6d3c79a4c0ed71c25fdfd350402bb2e78e42e8.patch?full_index=1"
+      sha256 "39472a04f1963a297713381a3a17e57183c1143a6602c194ca3016244caa6a9f"
+    end
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   bottle do
-    sha256 "3f5166d461e19f8e6eb838215ba1502fb6ec039a94cdab3d88a5ccdf62c675db" => :mojave
-    sha256 "102d8b6e9635321a7585d79c8c3c95d0f973c91cbf031be4d6839cf10c06ad2d" => :high_sierra
-    sha256 "45949916c354f6c3ba50df8ada5690f36d15ca1114185f1d92f66c4b08110f63" => :sierra
-    sha256 "df1f1a4b7efa73d48ada9d97ec13983fd1ba674773a058f771044dcd841a4b79" => :el_capitan
-    sha256 "d544abc34f9ed56f76fae104b8a472fe081c5072e32aeddbbd674316e9c0931d" => :yosemite
-    sha256 "83183c2346ec3642b45a20e47439225d94a58d6617669dd2001922f12d544942" => :mavericks
+    sha256 arm64_monterey: "d4aca71123a29f19c2ef454498d9c42fa2e82f87cab985470f8245a6f0528247"
+    sha256 arm64_big_sur:  "21a4e51be5f36088e2e1c1b882453de2b2ea367f6a5e477e58d28d4a0a842a78"
+    sha256 monterey:       "9bf13b79acc02f161664ae7995f427470a7e853abef9a8a0d1ca6aa6655fca12"
+    sha256 big_sur:        "bc77e15ea9074a9f3c555f30f1eb60c2a8e718ae40290c47648493e771d7cb84"
+    sha256 catalina:       "cd132007fa7178a543f11136afc85ae46dd98af743a23ed625d03c070b57f211"
+    sha256 x86_64_linux:   "97499d28a03419960360fcf6445e5a1f7778d94d5072daf7b426af6d802d39f8"
   end
 
   head do
-    url "https://github.com/networkupstools/nut.git"
+    url "https://github.com/networkupstools/nut.git", branch: "master"
     depends_on "asciidoc" => :build
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libusb-compat"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
-  conflicts_with "rhino", :because => "both install `rhino` binaries"
+  conflicts_with "rhino", because: "both install `rhino` binaries"
 
-  def caveats; <<~EOS
-    NUT requires configuration at #{etc}/nut to start.
-  EOS
-  end
-
-  plist_options :manual => "nut"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>WorkingDirectory</key>
-      <string>#{HOMEBREW_PREFIX}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>/usr/local/sbin/upsmon</string>
-        <string>-D</string>
-      </array>
-      <key>OnDemand</key>
-      <false/>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-      <key>ProcessType</key>
-      <string>Interactive</string>
-      <key>StandardOutPath</key>
-      <string>/usr/local/var/log/upsmon.log</string>
-      <key>StandardErrorPath</key>
-      <string>/usr/local/var/log/upsmon.log</string>
-    </dict>
-    </plist>
-  EOS
-  end
+  # plist_options :manual => "nut"
+  # def plist; <<~EOS
+  #   <?xml version="1.0" encoding="UTF-8"?>
+  #   <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  #   <plist version="1.0">
+  #   <dict>
+  #     <key>Label</key>
+  #     <string>#{plist_name}</string>
+  #     <key>WorkingDirectory</key>
+  #     <string>#{HOMEBREW_PREFIX}</string>
+  #     <key>ProgramArguments</key>
+  #     <array>
+  #       <string>/usr/local/sbin/upsmon</string>
+  #       <string>-D</string>
+  #     </array>
+  #     <key>OnDemand</key>
+  #     <false/>
+  #     <key>RunAtLoad</key>
+  #     <true/>
+  #     <key>KeepAlive</key>
+  #     <true/>
+  #     <key>ProcessType</key>
+  #     <string>Interactive</string>
+  #     <key>StandardOutPath</key>
+  #     <string>/usr/local/var/log/upsmon.log</string>
+  #     <key>StandardErrorPath</key>
+  #     <string>/usr/local/var/log/upsmon.log</string>
+  #   </dict>
+  #   </plist>
+  # EOS
+  # end
 
   def install
     if build.head?
-      ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+      ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
       system "./autogen.sh"
+    else
+      # Regenerate configure, due to patch applied
+      system "autoreconf", "-i"
     end
 
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--localstatedir=#{var}",
-                          "--with-macosx_ups",
-                          "--with-openssl",
-                          "--with-serial",
-                          "--with-usb",
-                          "--without-avahi",
-                          "--without-cgi",
-                          "--without-dev",
-                          "--without-doc",
-                          "--without-ipmi",
-                          "--without-libltdl",
-                          "--without-neon",
-                          "--without-nss",
-                          "--without-powerman",
-                          "--without-snmp",
-                          "--without-wrap",
-                          "--sysconfdir=#{etc}/nut"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --localstatedir=#{var}
+      --sysconfdir=#{etc}/nut
+      --with-statepath=#{var}/state/ups
+      --with-pidpath=#{var}/run
+      --with-systemdtmpfilesdir=#{pkgshare}
+      --with-macosx_ups 
+      --with-openssl
+      --with-serial
+      --with-usb
+      --without-avahi
+      --without-cgi
+      --without-dev
+      --without-doc
+      --without-ipmi
+      --without-libltdl
+      --without-neon
+      --without-nss
+      --without-powerman
+      --without-snmp
+      --without-wrap
+    ]
+    args << if OS.mac?
+      "--with-macosx_ups"
+    else
+      "--with-udev-dir=#{lib}/udev"
+    end
 
+    system "./configure", *args
     system "make", "install"
+  end
+
+  def post_install
+    (var/"state/ups").mkpath
+    (var/"run").mkpath
+  end
+
+  service do
+    run [opt_sbin/"upsmon", "-D"]
   end
 
   test do
