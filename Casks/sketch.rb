37c450b1497a8c4283cc5cdda75f80078416e6f1
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-12-13 11:52:42 -0600

    Fix Sketch cask for MacOS Monterey (12.7.4)
    + From
      https://raw.githubusercontent.com/Homebrew/homebrew-cask/2704f6569b65fe21a054bc82d10b03f3a8df6494/Casks/s/sketch.rb

---
+++ Casks/sketch.rb
@@ -0,0 +1,57 @@
+cask "sketch" do
+  on_big_sur :or_older do
+    version "96.3,167315"
+    sha256 "3ba2c147aab6b2fcb4b5cc3e3ee7fce3f63551e6ef743a7afe459bd0a87bb4a6"
+
+    url "https://download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_ventura :or_newer do
+    version "101.6,182078"
+    sha256 "30bcd1bbde8aa493592402d9fdceb5167543b440ee0954d451b6b5292a14cdd7"
+
+    url "https://download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://download.sketch.com/sketch-versions.xml"
+      strategy :sparkle
+    end
+  end
+  on_monterey do
+    version "100.3,180165"
+    sha256 "30bcd1bbde8aa493592402d9fdceb5167543b440ee0954d451b6b5292a14cdd7"
+    # https://download.sketch.com/sketch-100.3-180165.zip
+    url "https://download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://download.sketch.com/sketch-versions.xml"
+      strategy :sparkle
+    end
+  end
+
+  name "Sketch"
+  desc "Digital design and prototyping platform"
+  homepage "https://www.sketch.com/"
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "Sketch.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*",
+    "~/Library/Application Support/com.bohemiancoding.sketch3",
+    "~/Library/Autosave Information/com.bohemiancoding.sketch3.plist",
+    "~/Library/Caches/com.bohemiancoding.sketch3",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3",
+    "~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies",
+    "~/Library/HTTPStorages/com.bohemiancoding.sketch3.binarycookies",
+    "~/Library/Logs/com.bohemiancoding.sketch3",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.plist",
+    "~/Library/Saved Application State/com.bohemiancoding.sketch3.savedState",
+  ]
+end
