368e56884566e58b8d56cb2ab84387013546c619
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-12-13 00:20:57 -0600

    Fixup script

--- scripts/install_steam.sh
+++ scripts/install_steam.sh
@@ -3,11 +3,23 @@
 #
 #
 
-brew install --cask steam
+if [ "$1" = "install" ]; then
+  brew install --cask steam
 
-diskutil apfs addVolume disk1 APFS Steam
+  diskutil apfs addVolume disk1 APFS Steam
 
-mv /Applications/Steam.app /Volumes/Steam/Steam.app
-ln -s /Volumes/Steam/Steam.app /Applications/Steam.app
-mv ~/Library/Application\ Support/Steam /Volumes/Steam/Library
-ln -s /Volumes/Steam/Library ~/Library/Application\ Support/Steam
+  mv /Applications/Steam.app /Volumes/Steam/Steam.app
+  ln -s /Volumes/Steam/Steam.app /Applications/Steam.app
+  if [ -e ~/Library/Application\ Support/Steam ]; then
+    mv ~/Library/Application\ Support/Steam /Volumes/Steam/Library
+    ln -s /Volumes/Steam/Library ~/Library/Application\ Support/Steam
+  fi
+else
+  mv /Applications/Steam.app /Volumes/Steam/Steam.app
+  ln -s /Volumes/Steam/Steam.app /Applications/Steam.app
+  if [ -e ~/Library/Application\ Support/Steam ]; then
+    mv ~/Library/Application\ Support/Steam /Volumes/Steam/Library
+    ln -s /Volumes/Steam/Library ~/Library/Application\ Support/Steam
+  fi
+fi
+
